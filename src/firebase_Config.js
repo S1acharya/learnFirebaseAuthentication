7932b575ce838d142cd7321337a75ae20dd641da
S1Acharya123 <saketacharya.08@gmail.com>
2021-06-09 15:01:57 +0530

    let's start

---
+++ src/firebase_Config.js
@@ -0,0 +1,18 @@
+// import firebase from 'firebase/app';
+// import "firebase/firestore";
+
+// var firebaseConfig = {
+//     apiKey: "AIzaSyD9ZErRsrp4RBpGud1WbLTqBjXGrz5WGCw",
+//     authDomain: "contact-manager-b33d9.firebaseapp.com",
+//     projectId: "contact-manager-b33d9",
+//     storageBucket: "contact-manager-b33d9.appspot.com",
+//     messagingSenderId: "1071467330856",
+//     appId: "1:1071467330856:web:d2c54041db8fe06ec2241d"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+
+//   const db = firebase.firestore();
+//   export {db};
+
+  
